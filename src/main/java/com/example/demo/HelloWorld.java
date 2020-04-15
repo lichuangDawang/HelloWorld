52ff7be3ebc27c6ba47cba3f1b6c5671832fb0f2
Administrator <lichungmxcz@126.com>
2020-04-15 10:14:39 +0800

    hot deployment001

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -10,8 +10,22 @@ public class HelloWorld {
     public String hello(){
         return "hello danjie";
     }
-
-
+    @GetMapping("/name")
+    public String getJson(){
+        return  "name:lichuang";
+    }
+    @GetMapping("/name1")
+    public String getJson1(){
+        return  "name:lichuang222";
+    }
+    @GetMapping("/name2")
+    public String getJson2(){
+        return  "name:lichuang333";
+    }
+    @GetMapping("/name4")
+    public String getJson4(){
+        return  "name:lichuang444";
+    }
 
 
 }
